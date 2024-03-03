c1db6d804ba88e2fa2fb906394f9b2812f480f15
ZXinL-CN <xinliang.zhu@qq.com>
2024-03-03 18:01:38 +0800

    feat: Optimize code structure.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
+// 引入必要的模块
 import axios from 'axios';
 import fs from 'fs-extra';
 import path from 'node:path';
 import chalk from 'chalk';
 import { Command } from 'commander';
 
+// 初始化命令行工具
 const program = new Command();
 program.version('1.0.0')
     .option('-u, --url <url>', 'figma file url')
@@ -13,116 +15,102 @@ program.version('1.0.0')
     .option('-i, --ignoreNodeName [ignoreNodeName]', 'ignore node name')
     .option('-s, --scale [scale]', 'A number between 0.01 and 4, the image scaling factor', 2)
     .option('-f, --format [format]', 'A string enum for the image output format, can be jpg, png, svg, or pdf', 'png')
-    .action((options) => {
+    .action(async (options) => {
         console.log('options:', options);
-        if (!options.url) {
-            console.error('url is required');
+        // 检查必要参数
+        if (!options.url || !options.accessToken) {
+            console.error('URL and accessToken are required');
             process.exit(1);
         }
-        if (!options.accessToken) {
-            console.error('accessToken is required');
-            process.exit(1);
-        }
-        const url = options.url;
-        const { fileId, pageId } = getFileIdByURL(url);
-        console.log('fileId: ' + fileId);
-        console.log('pageId: ' + pageId);
-        options['fileId'] = fileId;
-        options['pageId'] = pageId;
 
-        // 调用下载函数
-        downloadFigmaInfo(options);
+        // 解析文件和页面ID
+        const { fileId, pageId } = getFileIdByURL(options.url);
+        console.log(`fileId: ${fileId}, pageId: ${pageId}`);
+        options.fileId = fileId;
+        options.pageId = pageId;
+
+        // 下载Figma信息
+        await downloadFigmaInfo(options);
     })
     .parse(process.argv);
 
+// 从URL中解析文件ID和页面ID
 function getFileIdByURL(urlStr) {
     const url = new URL(urlStr);
-    console.log('url: ', url);
     const pathname = url.pathname;
     const fileId = pathname.split('/file/')[1].split('/')[0];
     const pageId = url.searchParams.get('node-id').split('-').join(':');
     return { fileId, pageId };
 }
 
+// 下载Figma文件信息
 async function downloadFigmaInfo(options) {
     const { fileId, pageId, accessToken, imageSavePath } = options;
     try {
         console.log(chalk.blue('开始下载文件信息...'));
         const response = await axios.get(`https://api.figma.com/v1/files/${fileId}`, {
-            headers: {
-                'X-Figma-Token': accessToken
-            }
+            headers: { 'X-Figma-Token': accessToken }
         });
 
         console.log(chalk.green('获取到Figma文件内容...'));
+        const fileContent = response.data.document;
 
-        const fileContent = response.data;
-        console.log('fileContent: ', fileContent.document);
-
-        const pages = fileContent.document.children.filter((item) => item.id === pageId);
-        const nodesToRender = pages.map((item) => item.children.filter((item) => item.type === 'FRAME')).flat(2);
-        console.log('nodesToRender: ', nodesToRender);
+        // 过滤出指定页面的FRAME类型节点
+        const nodesToRender = fileContent.children
+            .filter((item) => item.id === pageId)
+            .flatMap((item) => item.children.filter((child) => child.type === 'FRAME'));
 
         console.log(chalk.yellow('检查目录是否存在，不存在则创建目录...'));
-        fs.ensureDir(imageSavePath)
-            .then(() => {
-                console.log(chalk.green(`Dirctory ${imageSavePath} exists or has been created.`));
-                // 在这里执行后续操作，如写入文件等
-                exportImages(nodesToRender, options);
-            })
-            .catch((err) => {
-                console.error(`Failed to create directory: ${err}`);
-            })
+        await fs.ensureDir(imageSavePath);
+        console.log(chalk.green(`目录 ${imageSavePath} 已存在或已创建。`));
+
+        // 导出图片
+        await exportImages(nodesToRender, options);
     } catch (error) {
-        console.error('Error fetching Figma file:', error);
+        console.error('下载Figma文件信息出错:', error);
     }
 }
 
-
+// 导出图片
 async function exportImages(nodesToRender, options) {
     if (!nodesToRender.length) {
-        console.log(chalk.red('<nodesToRender> must be specified.'));
+        console.log(chalk.red('必须指定要渲染的节点。'));
         process.exit(1);
-    };
+    }
+
     const { fileId, accessToken, scale, imageSavePath, format } = options;
     try {
         console.log(chalk.blue('开始导出图片...'));
 
         // 组织请求参数
         const queryParams = nodesToRender.map((item) => item.id).join(',');
-        console.log('queryParams: ', queryParams);
         const nodesMap = Object.fromEntries(nodesToRender.map((node) => [node.id, node]));
-        console.log('nodesMap: ', nodesMap);
 
-        // 发起 GET 请求
+        // 获取图片URL
         const imageUrl = `https://api.figma.com/v1/images/${fileId}?ids=${queryParams}&scale=${scale}&format=${format}`;
-        console.log(chalk.green('imageUrl: ', imageUrl));
+        const response = await axios.get(imageUrl, { headers: { 'X-Figma-Token': accessToken } });
 
-        const response = await axios.get(imageUrl, {
-            headers: {
-                'X-Figma-Token': accessToken
-            },
-        });
-
-        // 遍历返回的图片映射，保存每个图片到本地
+        // 保存图片到本地
         const imageMap = response.data.images;
-
         for (const [nodeId, imageUrl] of Object.entries(imageMap)) {
             if (imageUrl) {
+                // 构造文件名并解析文件路径
                 const fileName = `${nodesMap[nodeId].name}__${nodeId.split(':').join('-')}__${Date.now()}.${format}`;
                 const filePath = path.resolve(imageSavePath, fileName);
+                // 保存图片到文件
                 await saveImageToFile(imageUrl, filePath);
-                console.log(chalk.magenta(`Node ${nodeId} image saved as <${fileName}>`));
+                console.log(chalk.magenta(`节点 ${nodeId} 的图片已保存为 <${fileName}>`));
             } else {
-                console.warn(chalk.bgRed(`Failed to  render image for node ${nodeId}`));
+                console.warn(chalk.bgRed(`节点 ${nodeId} 的图片渲染失败`));
             }
         }
-        console.log(chalk.bgGreen('导出图片完成...'));
+        console.log(chalk.bgGreen('图片导出完成...'));
     } catch (error) {
-        console.error('Error fetching images: ', error);
+        console.error('导出图片时出错: ', error);
     }
 }
 
+// 将图片保存到文件
 async function saveImageToFile(imageUrl, filePath) {
     const response = await axios({
         url: imageUrl,
@@ -130,9 +118,10 @@ async function saveImageToFile(imageUrl, filePath) {
         responseType: 'stream'
     });
 
+    // 使用Promise确保图片完全写入
     await new Promise((resolve, reject) => {
         response.data.pipe(fs.createWriteStream(filePath))
             .on('finish', resolve)
-            .on('error', reject)
-    })
+            .on('error', reject);
+    });
 }
